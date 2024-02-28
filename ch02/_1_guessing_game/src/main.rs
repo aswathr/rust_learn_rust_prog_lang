03add1f07286cc029493f420be3363779b747302
Aswath <aswathrce@gmail.com>
2024-02-28 23:55:50 +0530

    Parse string to integer

--- ch02/_1_guessing_game/src/main.rs
+++ ch02/_1_guessing_game/src/main.rs
@@ -1,5 +1,6 @@
 use std::io; // import statement; [std] -> standard library; [io] -> input/output library
 use rand::Rng; // Rng is a trait and apparently we need to import it for implementers of the trait to be used in this file
+use std::cmp::Ordering; // an enum that we'll use
 
 fn main() {
     println!("Guess the number!");
@@ -17,5 +18,16 @@ fn main() {
     .read_line(&mut guess) // `read_line` gets input from the standard input; & -> reference; &mut -> mutable reference; `&mut guess` passes a mutable reference to the variable `guess`; `read_line` appends the input to the `String` argument passed
     .expect("Failed to read line"); // `read_line` returns a `Result` type (an enum whose values are `Ok` or `Err`); if the result is a error, this line will crash the program, but display the `msg`
 
+    let guess: u32 = guess // the string we're converting into a `u32`; the new `guess` `u32` variable shadows the old `guess` `String` variable
+    .trim() // removing whitespaces (like `\n`) and returns a `&str` slice
+    .parse() // converts string to the type that is specifiec; returns a `Result<u32, ParseIntError>` type that is Ok if the `&str` from the previous line could be converted to a `u32`; BTW, the error type is actually defined for integer types in `core/num/mod.rs` at the line `macro_rules! from_str_radix_int_impl {`
+    .expect("Please type a number!"); // this converts the `Result<>` into the underlying type `u32`
+
     println!("You guessed: {guess}"); // `{<variable>}` will insert the variable's value in place of `{<variable>}`
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small"),
+        Ordering::Equal => println!("You win"),
+        Ordering::Greater => println!("Too big"),
+    }
 }
