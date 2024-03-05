193e005f28d034e1da8598662a7ac8e8477a6714
Aswath <aswathrce@gmail.com>
2024-03-06 01:27:57 +0530

    comments chapter

---
+++ ch03/_4_comments/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // this is a comment
+
+    // multiple lines of comment need "//" at
+    // the beginning of each line of comment
+    println!("Hello, world!");
+
+    // there are other types of comments that are explained later in the book
+}
