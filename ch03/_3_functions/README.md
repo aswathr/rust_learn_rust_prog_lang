b1cb72f79a15af0b5d31f06b460c563dd7ff2df8
Aswath <aswathrce@gmail.com>
2024-03-06 01:25:43 +0530

    functions chapter completed

--- ch03/_3_functions/README.md
+++ ch03/_3_functions/README.md
@@ -1,4 +1,12 @@
 
 ### Functions
 
-the `main` function is the entry point for many programs
+a simple function signature `fn hello_world()`<br>
+the `main` function is the entry point for many programs<br>
+by convention, snake_case is used in naming functions<br>
+Rust doesn't care about order in which functions are defined<br>
+
+statements: instructions that perform some actions and doesn't return a value (for eg. `let x = 5` or `x = x + 2`)<br>
+expressions: evaluates to a resultant value (for eg. `{ let x = 9; x * 5 }` or `String::new()`)<br>
+
+function bodies contain a series of statements and may end with a expression<br>
--- ch03/_3_functions/src/main.rs
+++ ch03/_3_functions/src/main.rs
@@ -1,9 +1,33 @@
 fn main() {
-    println!("Hello, world!");
-    another_fn();
+    another_fn(5);
+    print_labeled_measurement(5, 'h');
+
+    fn invalid_assignment() {
+        // let x = (let y = 6); // The assignment statement `let y = 6` does not return a value, which is not the case in C
+    }
+
+    let y = {
+        // This is a valid assignment
+        let x = 6;
+        x + 5 // because this expression returns a value
+    };
+
+    // a function with return type
+    fn five() -> i32 {
+        // 5; // with a semicolon at the end of the `5` this becomes a statement and not a return expression
+        5 // this is a valid expression, that is returned
+    }
+
+    // which can be called and assigned to a variable like so
+    let f = five();
 }
 
 //use snake_case while naming functions
-fn another_fn() {
-    println!("another fn");
+fn another_fn(x: i32) {
+    println!("x is {x}");
+}
+
+//fn with two parameters
+fn print_labeled_measurement(value: i32, label: char) {
+    println!("The measurement is: {value}{label}");
 }
